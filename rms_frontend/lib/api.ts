dafed8636e430c7e8a19099395d7b80c26b12313
AsirMahmud <131938070+AsirMahmud@users.noreply.github.com>
2026-05-03 20:29:32 +0600

    Update api.ts

--- rms_frontend/lib/api.ts
+++ rms_frontend/lib/api.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
-
+const baseUrl= process.env.NEXT_PUBLIC_BASEURL || 'http://localhost:8000/api'
+console.log("Base Url:",baseUrl)
 export const api = axios.create({
-    baseURL: process.env.NEXT_PUBLIC_BASEURL || 'http://localhost:8000/api',
+    baseURL:baseUrl,
     headers: {
         'Content-Type': 'application/json',
     },
